2964b17c61cf5b96bca5b558da1bb93c578778a4
AngelahMbula <mbula.angelah@gmail.com>
2023-09-28 18:38:25 +0300

    routes arent working

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
+import React from "react";
 import { Route, Routes } from "react-router-dom";
-import Contact from "./pages/Contact";
-import About from "./pages/about";
+import Contact from "./pages/ContactUs";
+import About from "./pages/About";
 import Docs from "./pages/Docs";
-import Developer from "./pages/Developer";
-import HeaderLinks from "./pages/HeaderLinks";
-import FooterLinks from "./pages/FooterLinks";
+import Developer from "./pages/Developers";
+import Header from "./pages/Header";
+import Footer from "./pages/Footer";
 
 function App() {
   return (
     <div>
-      <HeaderLinks />
+      <Header  />
       <Routes>
         <Route path="/" element={<Docs />} />
         <Route path="/developer" element={<Developer />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
-      <FooterLinks />
+      <Footer />
     </div>
   );
 }
+
+export default App;
