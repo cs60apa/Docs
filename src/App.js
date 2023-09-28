0bbc49bd6905ee8f666c185ccb7a2ffc5c4d3080
marksikaundi <emmanuelmark117@gmail.com>
2023-09-28 11:22:02 +0200

    pth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import Contact from './pages/Contact';
-import About from './pages/about';
-import Developer from './pages/Developer';
-import HeaderLinks from './pages/HeaderLinks';
-import FooterLinks from './pages/FooterLinks';
-
+import { Route, Routes } from "react-router-dom";
+import Contact from "./pages/Contact";
+import About from "./pages/about";
+import Docs from "./pages/Docs";
+import Developer from "./pages/Developer";
+import HeaderLinks from "./pages/HeaderLinks";
+import FooterLinks from "./pages/FooterLinks";
 
 function App() {
   return (
     <div>
       <HeaderLinks />
       <Routes>
-        <Route path="/" element={<Developer />} />
+        <Route path="/" element={<Docs />} />
+        <Route path="/developer" element={<Developer />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
